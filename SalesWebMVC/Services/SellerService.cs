3bd8bc86f2381ae6a459764a9c536e4f72f3aa73
Letícia Carolina Sousa de Lima <leticiacarolinasdl@gmail.com>
2021-10-28 10:35:03 -0300

    Referential integrity

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -21,6 +21,7 @@ namespace SalesWebMVC.Services
 
         public void Insert(Seller thisObj)
         {
+            thisObj.Department = _context.Department.First();
             _context.Add(thisObj);
             _context.SaveChanges();
         }
